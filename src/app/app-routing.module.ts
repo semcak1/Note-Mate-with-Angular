af52629f31ff940677ced50ee1869c38c97b7be0
semcak1 <38374895+semcak1@users.noreply.github.com>
2020-08-15 14:42:42 +0300

    6 adding and updating  operations were done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainLAyoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
 
 const routes: Routes = [
   
   {path:'', component:MainLAyoutComponent, children:[
-    {path:'', component:NotesListComponent}
+    {path:'', component:NotesListComponent},
+    {path:'new',component:NoteDetailsComponent} ,{path:':id',component:NoteDetailsComponent}  
   ]}
 ];
 
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/shared/note.module';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-notes-list',
@@ -6,12 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes-list.component.scss']
 })
 export class NotesListComponent implements OnInit {
+  notes :Note[]=new Array<Note>();
 
   cardTitle:string='deneme1';
   bodyContent:string='asdasd lorem5'
-  constructor() { }
+  constructor(private notesService:NotesService) { }
 
   ngOnInit() {
+
+    //we want to retrieve all notes from NotesService
+    this.notes= this.notesService.getAll();
   }
 
 }
